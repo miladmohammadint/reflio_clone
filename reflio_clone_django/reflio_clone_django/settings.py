2bf5dbf7379cf3e6420b6ec77973be33729a9457
Milad Mohammadi <46770555+mm7213@users.noreply.github.com>
2024-03-23 18:49:13 +0100

    Create Django app for Reflio app and set Django DB engine

--- reflio_clone_django/reflio_clone_django/settings.py
+++ reflio_clone_django/reflio_clone_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    reflio_django_app,
+
 ]
 
 MIDDLEWARE = [
@@ -75,7 +77,7 @@ WSGI_APPLICATION = 'reflio_clone_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
---
+++ reflio_clone_django/reflio_django_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReflioDjangoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reflio_django_app'
