badc2cd78f381363eb00bb97e46af6d55800b718
Milad Mohammadi <46770555+mm7213@users.noreply.github.com>
2024-03-24 19:21:37 +0100

    Correct settings.py Django app reference and refer Supabase DB connection to .env file

--- reflio_clone_django/reflio_clone_django/settings.py
+++ reflio_clone_django/reflio_clone_django/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import environ
+env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    reflio_django_app,
+    'reflio_django_app',
 
 ]
 
@@ -76,10 +78,7 @@ WSGI_APPLICATION = 'reflio_clone_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
 }
 
 
