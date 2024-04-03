4e146f111c31e0a5a5c2e30eada149f342680fab
Milad Mohammadi <46770555+mm7213@users.noreply.github.com>
2024-04-03 23:12:36 +0200

    Fix home page loading
    Was routing to backend url not existing

--- apps/reflio/middleware.ts
+++ apps/reflio/middleware.ts
@@ -8,5 +8,5 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl;
   // Redirect all requests to the /api/auth endpoint
   url.pathname = '/api/v1/auth';
-  return NextResponse.rewrite(url);
+  return NextResponse.next();
 }
